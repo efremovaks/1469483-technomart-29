622299328342ceeddef7f254b3b16fdabfd167fe
efremovaks <65113288+efremovaks@users.noreply.github.com>
2020-10-12 11:38:51 +0300

    Декоративные элементы на главной странице

--- js/buy.js
+++ js/buy.js
@@ -1,8 +1,14 @@
 var BuyButton = document.querySelector(".button-buy");
 var BuyModal = document.querySelector(".buy-modal");
+var BuyModalClose = BuyModal.querySelector(".buy-modal-close");
 
 
 BuyButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     BuyModal.classList.add("buy-modal-show");
+  });
+
+  BuyButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    BuyModal.classList.remove("buy-modal-show");
   });
