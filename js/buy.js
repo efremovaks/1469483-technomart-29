8d5f86d34d89b67fb6bcd76b9af0691af64d1001
efremovaks <65113288+efremovaks@users.noreply.github.com>
2020-10-12 11:59:35 +0300

    Декоративные элементы на главной странице
    исправлена ошибка закрытия окна

--- js/buy.js
+++ js/buy.js
@@ -8,7 +8,16 @@ BuyButton.addEventListener("click", function (evt) {
     BuyModal.classList.add("buy-modal-show");
   });
 
-  BuyButton.addEventListener("click", function (evt) {
+  BuyModalClose.addEventListener("click", function (evt) {
     evt.preventDefault();
     BuyModal.classList.remove("buy-modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (BuyModal.classList.contains("buy-modal-show")) {
+        evt.preventDefault();
+        BuyModal.classList.remove("buy-modal-show");
+      }
+    }
   });
